a228cf99c6f4f56b4a76cc67cf2cdc4d350f24d0
Danielle Dias <danielle.c.santos9@aluno.senai.br>
2025-08-12 15:04:11 -0300

    :tada: Enviando projeto com a tela de consulta em processo de desenvolvimento

---
+++ app/index.jsx
@@ -0,0 +1,55 @@
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import { Input } from '../Components/input/input';
+export default function Index() {
+  return (
+    <>
+    {/* 1. Logo + imagem de fundo */}
+    {/* imagem de fundo */}
+    <ImageBackground source={require('../assets/images/imgFundo.png')}
+    style={styles.imgFundo}>
+      <Image source={require('../assets/images/Logo.png')}
+    style={styles.logo}></Image>
+    
+
+    </ImageBackground>
+    {/* 2. Campo de consulta */}
+    <View style={styles.container}>
+    {/* 2.1. Titulo */}
+    <Text>Consulte o seu CEP</Text>
+    {/* 2.2. input */}
+   <Input/>
+
+    {/* 2.3. botao */}
+    {/* 2.4. card de informacoes */}
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+imgFundo:{
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100%'
+},
+
+logo:{
+  width: 100,
+  height:120
+},
+
+container:{
+  flex:1.5,
+  alignItems: "center",
+  paddingTop: 50,
+  paddingBottom: 50,
+  gap: 40
+},
+
+titulo: {
+  fontSize: 25
+}
+
+})
